ed29bf779f6ff462239139ac6d6bf9be43813999
Aarav619 <aaravmishra619@gmail.com>
2022-04-08 23:36:22 +0530

    fix: lint & format code

--- Algorithms/quickSort.c
+++ Algorithms/quickSort.c
@@ -1,57 +1,62 @@
 #include <stdio.h>
 
-void swapnums(int* a, int* b){
-	int temp = *a;
+void swapnums(int *a, int *b)
+{
+	int t = *a;
 	*a = *b;
-	*b = temp;
+	*b = t;
 }
 
-int partition(int arr[], int l, int h){
+int partition(int arr[], int l, int h)
+{
 	int pivot = arr[l];
 	int i = l;
 	int j = h;
-	while( i < j){
-		while(arr[i] <= pivot){
+	while (i < j)
+	{
+		while (arr[i] <= pivot)
 			i++;
-		}
-		while(arr[i] > pivot){
+		while (arr[j] > pivot)
 			j--;
-		}
-		if(i < j){
+		if (i < j)
 			swapnums(&arr[i], &arr[j]);
-		}
-		swapnums(&arr[j], &arr[l]);
 	}
-	return j;
+	swapnums(&arr[j], &arr[l]);
+	return pivot;
 }
 
-int quickSort(int arr[], int l, int h){
-	if(l < h){
+int quickSort(int arr[], int l, int h)
+{
+	if (l < h)
+	{
 		int pivot = partition(arr, l, h);
-		quickSort(arr, l, pivot-1);
-		quickSort(arr, pivot+1, h);
+		quickSort(arr, l, pivot - 1);
+		quickSort(arr, pivot + 1, h);
 	}
 }
 
-int main(){
+int main()
+{
 	int n = 0;
-	printf("Enter size for the array: ");
+	printf("\nEnter size for the array: ");
 	scanf("%d", &n);
 
-	int h = n - 1, l=0;
+	int l = 0;
 	int arr[n];
 
-	printf("Enter elements for array: ");
-	for(int i=0; i<n; i++){
+	printf("\nEnter elements for array: ");
+	for (int i = 0; i < n; i++)
 		scanf("%d", &arr[i]);
-	}
 
-	quickSort(arr, l, h);
-
-	printf("Sorted array is: \n");
-	for(int i = 0; i<n; i++){
+	printf("\nThe array elements before sorting are: ");
+	for (int i = 0; i < n; i++)
 		printf("%d\t", arr[i]);
-	}
 
-return 0;
+	quickSort(arr, l, n - 1);
+
+	printf("\n\nSorted array is: ");
+	for (int i = 0; i < n; i++)
+		printf("%d\t", arr[i]);
+
+	return 0;
 }
